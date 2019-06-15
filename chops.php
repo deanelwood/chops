fbafeab057c385b4dfeaf202e665ae7cc316527e
Dean Elwood <dean.elwood@gmail.com>
2019-06-15 07:58:57 +0100

    Adding some common files

--- chops.php
+++ chops.php
@@ -6,44 +6,42 @@
     (c) Dean Elwood 2019
 
     Tail a log and push ERRORS and cummulative WARNings into Slack channel
-    Ignore DEBUG
-
 */
 
-/*
-    Config for your WebHook
-*/
-$webHookURL = "https://hooks.slack.com/services/*****************************";
-$logFile    = "/var/log/********************";
+include_once("common/configuration.php");
+include_once("common/logging.php");
+include_once("common/slack.php");
 
-/*
-    Config for WARNing messages in logfiles
-    In this current configuration, 3 WARNings in the space of 5 minutes will send an alarm
-*/
-$warningsBeforeAlarm = 3; // How many warnings in the window before we trigger an alarm
-$warningsWindow      = 5; // Number of minutes window for warnings
 $firstWarning        = 0;
 $warningCounter      = 0;
 
-$server = php_uname("n");
+$handle = popen("tail -f -n 0 "._LOG_FILE_TAIL." 2>&1", 'r');
 
-$handle = popen("tail -f -n 0 $logFile 2>&1", 'r');
+$warning = false;
+$error   = false;
 
-while(!feof($handle)){
-    $line = fgets($handle);
-    
-    // Lose any newlines before we POST to Slack....
-    $line = preg_replace('/\n$/','',$line);
+logThis("I am now monitoring : `"._LOG_FILE_TAIL."` for patterns ".str_replace("/", "`", _WARN_PATTERN)." and ".str_replace("/", "`", _ERROR_PATTERN), _INFO);
 
-    if (strpos($line, 'DEBUG') !== false) {
-        $line = "";
+while(!feof($handle)) {
+    $line    = fgets($handle);
+
+    $warning = preg_match(_WARN_PATTERN,  $line);
+    $error   = preg_match(_ERROR_PATTERN, $line);
+
+    // If don't have a match, move on...
+    if (!$warning && !$error) {
+        continue;
     }
+
+    // Lose any newlines before we POST to Slack....
+    $line = preg_replace('/\n$/', '', $line);
 
     /*
         If it's a warning, see if we have exceeded our threshold
     */
     $timeNow = time();
-    if (strpos($line, 'WARN') !== false) {
+
+    if ($warning) {
         // Probably a warning...
 
         if ($warningCounter == 0) {
@@ -54,43 +52,30 @@ while(!feof($handle)){
         // Increment our counter
         $warningCounter++;
 
-        if ($warningCounter < $warningsBeforeAlarm) {
+        if ($warningCounter < _WARNINGS_BEFORE_ALARM) {
             // eat it - no need to bother anyone
             $line = "";
         } else {
-            if (time() < ($firstWarning + ($warningsWindow * 60))) {
+            if (time() < ($firstWarning + (_WARNINGS_WINDOW * 60))) {
                 // We've had warningsBeforeAlarm number of warnings within warningsWindow, tell the team.
                 // Nothing to do, $line will not be blank and message will be sent...
             } else {
                 // Time exceeded, reset by treating this as if it were the first warning
-                $firstWarning = time();
+                $firstWarning   = time();
                 $warningCounter = 1;
-                $line = "";
+                //$line           = "";
+                continue;
             }
         }
     }
 
-    if ($line != "") {
-        $opts = array(
-            'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            ),
-
-            'http' => array(
-                'method' => 'POST',
-                'header' => 'Content-type: application/json',
-                'content' => '{"text":"['.$server.'] '.$line.'"}'
-            )
-        );
-
-        $context = stream_context_create($opts);
-
-        $response = file_get_contents($webHookURL, false, $context);
+    if (!postToSlackChannel(_SERVER, $line, _SLACK_WEB_HOOK_URL)) {
+        logThis("Failed to post message into Slack channel : $line", _ERROR);
     }
 
     flush();
-    usleep(500000); // 0.5 second.... (so max 2 message per second to Slack)
+
+    usleep(1000000/_MESSAGES_PER_SECOND);
 }
 
 pclose($handle);
